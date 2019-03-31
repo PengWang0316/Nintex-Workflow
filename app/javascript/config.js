8bcf74e0dff9470b8b3585662dcadafb6dd71b7a
Kevin Wang <rocwang0316@gmail.com>
2019-03-31 12:17:24 -0700

    Added an id for the search button.

--- app/javascript/config.js
+++ app/javascript/config.js
@@ -3,3 +3,5 @@ export const NWC_URL_API_KEY = 'nintex-nwc-url-key';
 export const WORKFLOW_TABLE_ID = '#workflowTable';
 export const NWC_LIST_WORKFLOWS_API = 'https://us.nintex.io/workflows/v1/designs';
 export const BEARER_HEADER = 'Bearer ';
+export const TOOLTIP_DATA_TOGGLE = '[data-toggle="tooltip"]';
+export const SEARCH_BTN_ID = '#searchBtn';
