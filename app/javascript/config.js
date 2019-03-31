e2c09a085518410c3f95c995baea1f27cdaaaecd
Kevin Wang <rocwang0316@gmail.com>
2019-03-31 11:43:29 -0700

    Added NWC_LIST_WORKFLOWS_API

--- app/javascript/config.js
+++ app/javascript/config.js
@@ -1,3 +1,4 @@
 export const NWC_ID = '#nwcContainer';
 export const NWC_URL_API_KEY = 'nintex-nwc-url-key';
 export const WORKFLOW_TABLE_ID = '#workflowTable';
+export const NWC_LIST_WORKFLOWS_API = 'https://us.nintex.io/workflows/v1/designs';
