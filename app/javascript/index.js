2b4c4a2a06798fde9719c7b876f548843d36f197
Kevin Wang <rocwang0316@gmail.com>
2019-03-31 15:02:50 -0700

    Rename the controller

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,12 +1,16 @@
-import initialNWC from './controllers/InitialNWC';
+import initialNWC from './controllers/NWCController';
 import searchWorkflows from './controllers/SearchWorkFlow';
 import { TOOLTIP_DATA_TOGGLE, SEARCH_BTN_ID } from './config';
+import { fillTable } from './views/WorkflowTableView';
 
 const handler = () => {
   // Initialize the tooltip
   $(TOOLTIP_DATA_TOGGLE).tooltip();
   // Initialize the NWC panel
   initialNWC();
+
+  // Initialize an empty table
+  fillTable([]);
 
   /* Register the listeners */
   // Search button
