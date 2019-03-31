d58abf379921553e366d7896cc1a168f764ffad9
Kevin Wang <rocwang0316@gmail.com>
2019-03-31 14:24:36 -0700

    Added Search btn id

--- app/javascript/config.js
+++ app/javascript/config.js
@@ -2,6 +2,6 @@ export const NWC_ID = '#nwcContainer';
 export const NWC_URL_API_KEY = 'nintex-nwc-url-key';
 export const WORKFLOW_TABLE_ID = '#workflowTable';
 export const NWC_LIST_WORKFLOWS_API = 'https://us.nintex.io/workflows/v1/designs';
-export const BEARER_HEADER = 'Bearer ';
+export const BEARER_HEADER = 'Bearer';
 export const TOOLTIP_DATA_TOGGLE = '[data-toggle="tooltip"]';
 export const SEARCH_BTN_ID = '#searchBtn';
