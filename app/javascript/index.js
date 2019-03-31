75e278daeeef8f04c3ec28b4101ab6f48f289df8
Kevin Wang <rocwang0316@gmail.com>
2019-03-31 14:25:33 -0700

    Reorgnized the code

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,4 +1,16 @@
 import initialNWC from './controllers/InitialNWC';
+import searchWorkflows from './controllers/SearchWorkFlow';
+import { TOOLTIP_DATA_TOGGLE, SEARCH_BTN_ID } from './config';
 
-$('[data-toggle="tooltip"]').tooltip();
-initialNWC();
+const handler = () => {
+  // Initialize the tooltip
+  $(TOOLTIP_DATA_TOGGLE).tooltip();
+  // Initialize the NWC panel
+  initialNWC();
+
+  /* Register the listeners */
+  // Search button
+  $(SEARCH_BTN_ID).on('click', 'img', searchWorkflows);
+};
+
+handler();
