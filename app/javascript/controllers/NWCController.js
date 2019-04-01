18ee1f3119b38b2afed638770605cb2acd226c39
Kevin Wang <rocwang0316@gmail.com>
2019-03-31 17:01:14 -0700

    Added the add and remove NWC api functionality.

--- app/javascript/controllers/NWCController.js
+++ app/javascript/controllers/NWCController.js
@@ -1,16 +1,52 @@
-import { fetchNWCApis, removeNWCApi } from '../models/NWC';
-import { initialNWCApiView, toggleRemoveAlert } from '../views/NWCApiView';
-import { DELETE_CONFIRM_BTN_ID } from '../config';
-
-export const initialNWC = () => {
-  const nwcKeys = fetchNWCApis();
-  initialNWCApiView(nwcKeys);
-};
+import { fetchNWCApis, removeNWCApi, addNWCApi } from '../models/NWC';
+import {
+  initialNWCApiView, toggleRemoveAlert, toggleAddModal, disableSaveBtn, enableSaveBtn, appendBadge,
+} from '../views/NWCApiView';
+import {
+  DELETE_CONFIRM_BTN_ID, ADD_NWC_BTN_ID, ADD_NWC_NAME_ID,
+  ADD_NWC_KEY_ID, ADD_NWC_CONFIRM_BTN_ID, RM_BTN_CLASS,
+} from '../config';
 
 export const removeNWCKey = ({ target }) => {
   // Bind the delete confirm button with the right key
   $(DELETE_CONFIRM_BTN_ID).attr('data-key', $(target).attr('data-key'));
   toggleRemoveAlert();
+};
+
+/**
+ * Check the name and key input to disable the confirm button when they are empty
+ */
+const checkInput = () => {
+  const name = $(ADD_NWC_NAME_ID).val().trim();
+  const key = $(ADD_NWC_KEY_ID).val().trim();
+  if (name !== '' && key !== '') enableSaveBtn();
+  else disableSaveBtn();
+};
+
+const addNwcApi = () => {
+  const nameElement = $(ADD_NWC_NAME_ID);
+  const keyElement = $(ADD_NWC_KEY_ID);
+  addNWCApi(nameElement.val(), keyElement.val());
+  appendBadge(nameElement.val());
+
+  // Add remove button listener
+  $(`.${RM_BTN_CLASS}`).on('click', removeNWCKey);
+
+  // Clear name and key input
+  nameElement.val('');
+  keyElement.val('');
+
+  toggleAddModal();
+};
+
+export const initialNWC = () => {
+  const nwcKeys = fetchNWCApis();
+  initialNWCApiView(nwcKeys);
+  // Add listeners
+  $(ADD_NWC_BTN_ID).on('click', toggleAddModal);
+  $(ADD_NWC_NAME_ID).on('input', checkInput);
+  $(ADD_NWC_KEY_ID).on('input', checkInput);
+  $(ADD_NWC_CONFIRM_BTN_ID).on('click', addNwcApi);
 };
 
 export const confirmRm = ({ target }) => {
--- app/javascript/views/NWCApiView.js
+++ app/javascript/views/NWCApiView.js
@@ -1,13 +1,23 @@
-import { NWC_ID, RM_ALERT_ID, RM_BTN_CLASS } from '../config';
+import {
+  NWC_ID, RM_ALERT_ID, RM_BTN_CLASS, ADD_NWC_API_ID, ADD_NWC_CONFIRM_BTN_ID,
+} from '../config';
 import styles from '../../css/ApiView.module.css';
 
+const getBadgeHtml = key => `<span class="${styles.badge}" id="nwc_${key}">
+                              ${key}<span data-key=${key} class="btn ml-2 ${styles.removeBtn} ${RM_BTN_CLASS}">X</span>
+                            </span>`;
+
 export const initialNWCApiView = (apiKeys) => {
-  const viewArr = Object.keys(apiKeys).map(key => `<span class="${styles.badge}" id="nwc_${key}">
-                                                    ${key}<span data-key=${key} class="btn ml-2 ${styles.removeBtn} ${RM_BTN_CLASS}">X</span>
-                                                  </span>`);
+  const viewArr = Object.keys(apiKeys).map(getBadgeHtml);
   $(NWC_ID).html(viewArr.join(''));
 };
 
-export const toggleRemoveAlert = () => {
-  $(RM_ALERT_ID).modal('toggle');
-};
+export const appendBadge = key => $(NWC_ID).append(getBadgeHtml(key));
+
+export const toggleRemoveAlert = () => $(RM_ALERT_ID).modal('toggle');
+
+export const toggleAddModal = () => $(ADD_NWC_API_ID).modal('toggle');
+
+export const disableSaveBtn = () => $(ADD_NWC_CONFIRM_BTN_ID).prop('disabled', true);
+
+export const enableSaveBtn = () => $(ADD_NWC_CONFIRM_BTN_ID).prop('disabled', false);
