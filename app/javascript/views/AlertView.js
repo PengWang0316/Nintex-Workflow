32468ae9852ba8dee3ec44105dc908992067acb3
Kevin Wang <rocwang0316@gmail.com>
2019-04-02 12:39:09 -0700

    Fixed a syntax error

--- app/javascript/views/AlertView.js
+++ app/javascript/views/AlertView.js
@@ -1,11 +1,12 @@
 import { ALERT_ELEMENT_ID } from '../config';
 
+let timeout;
+
 export const toggleAlert = (text) => {
-  let timeout;
   const alertElement = $(ALERT_ELEMENT_ID);
   alertElement.text(text);
   alertElement.slideToggle('fast', 'linear', () => {
-    if (timeout) timeout.clearTimeout();
+    if (timeout) clearTimeout(timeout);
     timeout = setTimeout(() => alertElement.slideToggle(), 3000);
   });
 };
