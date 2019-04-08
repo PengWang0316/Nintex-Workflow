b9cfe4be8b23a5eeb5ea18b11c4a25e668eca913
Kevin Wang <rocwang0316@gmail.com>
2019-04-08 14:46:27 -0700

    Switched the async and await to two promise calls with a manually check to make sure the http calls will be issued parallelly.

--- app/javascript/controllers/SearchWorkFlowController.js
+++ app/javascript/controllers/SearchWorkFlowController.js
@@ -3,13 +3,32 @@ import { fetchWorkflows as fetchOfficeWorkflows } from '../models/Office';
 import { fillTable } from '../views/WorkflowTableView';
 import { disableLoading, enableNormal } from '../views/SearchBtnView';
 
-export const searchWorkflows = async () => {
+export const searchWorkflows = () => {
   disableLoading();
-  const nwcData = await fetchWorkflows();
-  const officeData = await fetchOfficeWorkflows();
-  fillTable([...nwcData, ...officeData]);
-  // fillTable(nwcData);
-  enableNormal();
+  // const nwcData = await fetchWorkflows();
+  // const officeData = await fetchOfficeWorkflows();
+  // fillTable([...nwcData, ...officeData]);
+  // enableNormal();
+
+  let nwcData;
+  let officeData;
+  // Use two promises and manually check result to make sure
+  // the http calls for different sources will be issued parallelly.
+  fetchWorkflows().then((data) => {
+    nwcData = data;
+    if (nwcData && officeData) {
+      fillTable([...nwcData, ...officeData]);
+      enableNormal();
+    }
+  });
+
+  fetchOfficeWorkflows().then((data) => {
+    officeData = data;
+    if (nwcData && officeData) {
+      fillTable([...nwcData, ...officeData]);
+      enableNormal();
+    }
+  });
 };
 
 export default searchWorkflows;
